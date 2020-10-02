4c814a7d234547d89e1fd9ff8b48d67f37b8d870
Evge-s <egoeny@gmail.com>
2020-10-02 16:01:22 +0300

    add db, login, register models/views

---
+++ Models/DataModels/DataContext.cs
@@ -0,0 +1,39 @@
+﻿using IFTurist.Models.UserModels;
+using Microsoft.EntityFrameworkCore;
+
+namespace IFTurist.Models.DataModels
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        public DataContext(DbContextOptions<DataContext> options)
+           : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            string adminRoleName = "admin";
+            string userRoleName = "user";
+
+            string adminEmail = "admin@gmail.com";
+            string adminPassword = "admin123";
+            string turistEmail = "user@gmail.com";
+            string turistPassword = "user123";
+
+            Role adminRole = new Role { Id = 1, Name = adminRoleName };
+            Role userRole = new Role { Id = 2, Name = userRoleName };
+
+            User adminUser = new User { Id = 1, Email = adminEmail, Password = adminPassword, RoleId = adminRole.Id };
+            User turistUser = new User { Id = 2, Email = turistEmail, Password = turistPassword, RoleId = userRole.Id };
+
+            modelBuilder.Entity<Role>().HasData(new Role[] { adminRole, userRole });
+            modelBuilder.Entity<User>().HasData(new User[] { adminUser, turistUser });
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ Models/DataModels/SampleData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IFTurist.Models.DataModels
+{
+    public class SampleData
+    {
+        // for seeding data future funcional
+        public static void Initialize(DataContext context)
+        {
+            context.SaveChanges();
+        }
+    }
+}
