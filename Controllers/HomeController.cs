9c9f64c1fb3376603675f62a9d07dd508d8fac8a
Evge-s <egoeny@gmail.com>
2020-10-11 18:58:47 +0300

    add Partial View for tours

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -22,7 +22,16 @@ namespace IFTurist.Controllers
         public IActionResult Index()
         {
             Tour tour = new Tour() { Id = 1, ImageLink = "/img/tours/yaremche/yaremche.jpg", Name = "Яремче" }; // for test
-            List<Tour> tours = new List<Tour> { tour };      
+            Tour tour2 = new Tour() { Id = 1, ImageLink = "/img/tours/yaremche/yaremche.jpg", Name = "122" };
+            Tour tour3 = new Tour() { Id = 1, ImageLink = "/img/tours/yaremche/yaremche.jpg", Name = "33" };
+            Tour tour4 = new Tour() { Id = 1, ImageLink = "/img/tours/yaremche/yaremche.jpg", Name = "33" };
+            Tour tour5 = new Tour() { Id = 1, ImageLink = "/img/tours/yaremche/yaremche.jpg", Name = "33" };
+            Tour tour6 = new Tour() { Id = 1, ImageLink = "/img/tours/yaremche/yaremche.jpg", Name = "33" };
+            Tour tour7 = new Tour() { Id = 1, ImageLink = "/img/tours/yaremche/yaremche.jpg", Name = "33" };
+            Tour tour8 = new Tour() { Id = 1, ImageLink = "/img/tours/yaremche/yaremche.jpg", Name = "33" };
+            Tour tour9 = new Tour() { Id = 1, ImageLink = "/img/tours/yaremche/yaremche.jpg", Name = "33" };
+            Tour tour10 = new Tour() { Id = 1, ImageLink = "/img/tours/yaremche/yaremche.jpg", Name = "33" };
+            List<Tour> tours = new List<Tour> { tour, tour2, tour3, tour4, tour5, tour6, tour7, tour8, tour9, tour10 };      
             return View(tours);
         }
 
