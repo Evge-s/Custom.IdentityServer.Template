48a86d57a38461ceed0d5a31d4b25233ae609d4a
Evge-s <egoeny@gmail.com>
2020-10-11 19:39:32 +0300

    add Admin keys to Partial Views

---
+++ Controllers/TourController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IFTurist.Controllers
+{
+    public class TourController : Controller
+    {
+        public IActionResult Edit()
+        {
+            return View();
+        }
+        public IActionResult Delete()
+        {
+            return View();
+        }
+
+    }
+}
